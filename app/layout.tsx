e684cd695dee03877769970d0e2f253e5368a122
Shu Mimura <124022614+ShuMimura@users.noreply.github.com>
2025-10-03 19:09:03 +0900

    Add Google Tag Manager (GTM-KJQP456N)

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,17 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KJQP456N');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -38,6 +49,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen bg-[#eaeded]`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KJQP456N"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Header />
         <main className="flex-1">
           {children}
